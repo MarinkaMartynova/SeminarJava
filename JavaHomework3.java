a3184935f5818272f98f9bc4aa92c56a5bfd6740
Marinka <saramsok@gmail.com>
2023-02-14 01:30:28 +0400

    add JavaHomework3.java

---
+++ JavaHomework3.java
@@ -0,0 +1,41 @@
+
+import java.util.Scanner;
+/*
+ * Задание 3:  Реализовать простой калькулятор
+ * 
+ */
+public class JavaHomework3 {
+    public static void main(String[] args) {
+                
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите первое число: ");
+        double num1 = scanner.nextDouble();
+        System.out.print("Введите второе число: ");
+        double num2 = scanner.nextDouble();
+        System.out.print("Арифметическое действие: ");
+        char operation = scanner.next().charAt(0);
+        scanner.close();
+        double result;
+        switch (operation) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                result = num1 / num2;
+                break;
+            default:
+                System.out.println("Неизвестная операция");
+                return;
+        }
+        System.out.printf("Ответ: " + num1 + " " + operation + " " + num2 + " = " + result);
+        
+       
+    }
+
+}    
