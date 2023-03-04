3f238668ec265df2c01213a42b44b2b0feb375c8
Marinka <saramsok@gmail.com>
2023-03-04 22:44:30 +0400

    update homework by lesson 5

--- homeworkByLesson5.java
+++ homeworkByLesson5.java
@@ -1,5 +1,6 @@
 import java.util.Arrays;
 import java.util.Random;
+import java.util.HashSet;
 
 /**
  * Учитывая два целочисленных массива nums1 и nums2, nums1 = [1,2,3,2,1], nums2 = [3,2,1,4,7]
@@ -51,23 +52,45 @@ public class homeworkByLesson5 {
         System.out.println(result);
                
     }
-    public static int findLength(int[] nums1, int[] nums2) {
-        int m = nums1.length;
-        int n = nums2.length;
-        int[][] dp = new int[m+1][n+1];
-        int maxLength = 0;
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (nums1[i-1] == nums2[j-1]) {
-                    dp[i][j] = dp[i-1][j-1] + 1;
-                    maxLength = Math.max(maxLength, dp[i][j]);
-                } else {
-                    dp[i][j] = 0;
-                }
-            }
+    public static int findLength(int[] nums1, int[] nums2){
+        HashSet<Integer> set1 = new HashSet<>();
+
+        for (int num : nums1) {
+            set1.add(num);
         }
-        return maxLength;
+        int maxLen = 0;
+        int len = 0;
+        for (int i = 0; i < nums2.length; i++) {
+          if (set1.contains(nums2[i])) {
+            len++;
+            maxLen = Math.max(maxLen, len);
+          } 
+          else {
+            len = 0;
+          }
+        }
+        
+        return maxLen;
     }
+
+    //второе решение 
+    // public static int findLength(int[] nums1, int[] nums2) {
+        // int m = nums1.length;
+        // int n = nums2.length;
+        // int[][] dp = new int[m+1][n+1];
+        // int maxLength = 0;
+        // for (int i = 1; i <= m; i++) {
+            // for (int j = 1; j <= n; j++) {
+                // if (nums1[i-1] == nums2[j-1]) {
+                    // dp[i][j] = dp[i-1][j-1] + 1;
+                    // maxLength = Math.max(maxLength, dp[i][j]);
+                // } else {
+                    // dp[i][j] = 0;
+                // }
+            // }
+        // }
+        // return maxLength;
+    // }
 
     
 }
